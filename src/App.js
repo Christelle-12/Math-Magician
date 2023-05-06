8ce8042f3ed27eca0e8024f62c8eaf9970cff2dd
Chritelle-12 <mariechristellenirere@gmail.com>
2023-05-06 07:46:21 +0200

    Fetched and displayed data using API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Calculator from './components/Calculator';
+import Quotes from './components/quotes';
 
 function App() {
   return (
     <div>
       <Calculator />
+      <Quotes />
     </div>
   );
 }
