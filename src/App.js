d0ff891f60c4c41c73fcbbc6c195024cfba2993e
Chritelle-12 <mariechristellenirere@gmail.com>
2023-05-09 11:40:03 +0200

    Routed Home,Calculator and Quotes pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Route, Link, Routes,
+} from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quotes from './components/quotes';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quotes />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li>
+            <Link to="/Calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
